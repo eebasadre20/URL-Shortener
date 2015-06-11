bec9d713b209cc29e7d90276152b9795ec8638ab
eebasadre20 <eebasadre20@gmail.com>
2015-06-12 01:47:25 +0800

    Add some styles

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
 
 
-  get '/:url' => 'links#redirect'
+  get '/:url' => 'links#redirect', as: 'redirect_url'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
